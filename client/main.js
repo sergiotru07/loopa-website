948e5c28719c3d11eb513b2bc67c3bf3d8f30fc5
sergiotru07 <sergiotrujillo.a@gmail.com>
2015-12-15 01:02:16 -0500

    Se creó una nueva estructura HTML para los dispositivos tablets y móviles

--- client/main.js
+++ client/main.js
@@ -2,17 +2,7 @@ $(document).ready(function() {
 	$(".button-collapse").sideNav();
 
 	$(window).resize(function(){
-		if ($(window).width() <= 992){	
-			$('#btn1').addClass('right-align').removeClass('center-align');
-			$('#btn2').addClass('left-align').removeClass('center-align');
-		}else{
-			$('#btn1').addClass('center-align').removeClass('right-align');
-			$('#btn2').addClass('center-align').removeClass('left-align');
-		}	
-	});
-
-	$(window).resize(function(){
-		if ($(window).width() <= 601){	
+		if ($(window).width() <= 1180){	
 			$('#tkc').addClass('btn').removeClass('btn-large');
 			$('#mkc').addClass('btn').removeClass('btn-large');
 		}else{
