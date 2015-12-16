20e268cc4f0bd505b14d67d764810d84bd9e13f9
Angarsk8 <andresa.garciah621@gmail.com>
2015-12-16 16:41:27 -0500

    Corregir sidenav cuando la resolución es mayor a 993px y refactorizar un poco el código en main.js

--- client/main.js
+++ client/main.js
@@ -1,26 +1,38 @@
-$(document).ready(function() {
-	$(".button-collapse").sideNav();
+Meteor.startup(function() {
 
-	$(window).resize(function(){
-		if ($(window).width() <= 1180){	
-			$('#tkc').addClass('btn').removeClass('btn-large');
-			$('#mkc').addClass('btn').removeClass('btn-large');
-		}else{
-			$('#tkc').addClass('btn-large').removeClass('btn');
-			$('#mkc').addClass('btn-large').removeClass('btn');
-		}		
-	});
-	
-	/*Agregar transformación del menú hamburguesa a flecha y la transición de los elementos del sidebar*/
-	$('.button-collapse').on('click', function(){
-		$('#arrowEfct').removeClass('material-design-hamburger__icon--from-arrow');
-		$('#arrowEfct').toggleClass('material-design-hamburger__icon--to-arrow');
-		$('.side-nav .animate').toggleClass('actived');
-	});		
+    function openSideNav() {
+        $('#arrowEfct').removeClass('material-design-hamburger__icon--from-arrow')
+            .toggleClass('material-design-hamburger__icon--to-arrow');
+        $('.side-nav .animate').toggleClass('actived');
+    }
 
-	$('.drag-target').on('click', function(){
-		$('#arrowEfct').removeClass('material-design-hamburger__icon--to-arrow');
-		$('#arrowEfct').toggleClass('material-design-hamburger__icon--from-arrow');
-		$('.side-nav li').removeClass('actived');
-	});	
-})
+    function closeSideNav() {
+        $('#arrowEfct').removeClass('material-design-hamburger__icon--to-arrow')
+            .toggleClass('material-design-hamburger__icon--from-arrow');
+        $('.side-nav li').removeClass('actived');
+    }
+
+    $(".button-collapse").sideNav();
+
+    $(window).resize(function() {
+
+        let sideNavLeft = parseInt($(".side-nav").css("left"));
+
+        if ($(this).width() >= 993 && sideNavLeft >= 0) {
+            $(".button-collapse").sideNav("hide")
+            closeSideNav();
+        }
+
+        if ($(this).width() <= 1180) {
+            $('#tkc').addClass('btn').removeClass('btn-large');
+            $('#mkc').addClass('btn').removeClass('btn-large');
+        } else {
+            $('#tkc').addClass('btn-large').removeClass('btn');
+            $('#mkc').addClass('btn-large').removeClass('btn');
+        }
+    });
+
+    /*Agregar transformación del menú hamburguesa a flecha y la transición de los elementos del sidebar*/
+    $('.button-collapse').on('click', () => { openSideNav() });
+    $('.drag-target').on('click', () => { closeSideNav() });
+});
