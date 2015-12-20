82dcf6699387ed096e63734081eba88618cfcf05
Angarsk8 <andresa.garciah621@gmail.com>
2015-12-20 17:32:32 -0500

    Add proper styles to website container

--- client/templates/website/website.js
+++ client/templates/website/website.js
@@ -1,9 +1,16 @@
 Session.set("isSidenavOpen", false);
+const containerStyles = `
+	overflow: hidden;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	width: 100%;
+`
 
 Template.website.helpers({
     isSidenavOpen: () => Session.get("isSidenavOpen"),
     darkBgDisplay: () => Session.get("isSidenavOpen") ? "block" : "none",
-    bodyOverflow: () => Session.get("isSidenavOpen") ? "hidden" : "none"
+    containerStyles: () => Session.get("isSidenavOpen") ? containerStyles : "none"
 });
 
 Template.website.events({
