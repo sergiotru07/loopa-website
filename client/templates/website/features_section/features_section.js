ae3edc7b5a8e422ab773f04f82c1c7aaa2c776df
sergiotru07 <sergiotrujillo.a@gmail.com>
2015-12-21 19:23:38 -0500

    fix bug in the features grid when resize the screen resolution

--- client/templates/website/features_section/features_section.js
+++ client/templates/website/features_section/features_section.js
@@ -1,8 +1,4 @@
-const [largeHeight, mediumHeight, smallHeight] = [
-    [550, 275, 275, 275, 350, 200],
-    [550, 275, 275, 275, 250, 250],
-    200
-];
+const cardTitleHeight = "50";
 
 let cardsContent = {
     firstCard: {
@@ -19,72 +15,38 @@ let cardsContent = {
     },
     thirdCard: {
         featureIndex: "feature-three",
-        color: "blue lighten-3",
+        color: "deep-purple lighten-2",
         featureTitle: "Feature Three",
         featureDescription: "Lorem ipsum dolor sit amet, mutat graeco, Lorem ipsum dolor sit amet, mutat graeco"
     },
     fourthCard: {
         featureIndex: "feature-four",
-        color: "deep-purple lighten-2",
+        color: "purple lighten-3",
         featureTitle: "Feature Four",
         featureDescription: "Lorem ipsum dolor sit amet, mutat graeco, Lorem ipsum dolor sit amet, mutat graeco"
     },
     fifthCard: {
         featureIndex: "feature-five",
-        color: "purple lighten-3",
+        color: "light-blue lighten-3",
         featureTitle: "Feature Five",
         featureDescription: "Lorem ipsum dolor sit amet, mutat graeco, Lorem ipsum dolor sit amet, mutat graeco"
-    },
-    sixthCard: {
-        featureIndex: "feature-six",
-        color: "light-blue lighten-3",
-        featureTitle: "Feature Six",
-        featureDescription: "Lorem ipsum dolor sit amet, mutat graeco, Lorem ipsum dolor sit amet, mutat graeco"
-    }
-};
-
-let _heightValidation = (name, heightArr) => {
-    if (name === "feature-one") {
-        return heightArr[0];
-    } else if (name === "feature-two") {
-        return heightArr[1];
-    } else if (name === "feature-three") {
-        return heightArr[2];
-    } else if (name === "feature-four") {
-        return heightArr[3];
-    } else if (name === "feature-five") {
-        return heightArr[4];
-    } else if (name === "feature-six") {
-        return heightArr[5];
-    }
-};
-
-let getTranslateHeight = (name) => {
-    let windowWidth = Session.get("windowWidth");
-    if (windowWidth >= 993) {
-        return _heightValidation(name, largeHeight);
-    } else if (windowWidth < 993 && windowWidth > 600) {
-        return _heightValidation(name, mediumHeight);
-    } else if (windowWidth <= 600) {
-        return smallHeight;
     }
 };
 
 Template.featuresSection.events({
     "mouseenter .card": function(e, t) {
-        let $target = $(e.target);
-        $target
-            .find(".card-back")
-            .css("transform", "translateY(0)");
-    },
-    "mouseleave .card": function(e, t) {
         let $target = $(e.target),
-            targetName = $target.attr("name"),
-            translateHeight = getTranslateHeight(targetName) - 70;
+            targetHeight = $target.height();
 
         $target
             .find(".card-back")
-            .css("transform", `translateY(${translateHeight}px)`);
+            .css("height", targetHeight);
+    },
+    "mouseleave .card": function(e, t) {
+        let $target = $(e.target)
+        $target
+            .find(".card-back")
+            .css("height", cardTitleHeight);
     }
 });
 
