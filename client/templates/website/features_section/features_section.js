582abd6f6daf519830588daeec0722ea96f0a1fc
sergiotru07 <sergiotrujillo.a@gmail.com>
2015-12-22 21:20:55 -0500

    Remove media query backgraound image courses cards

--- client/templates/website/features_section/features_section.js
+++ client/templates/website/features_section/features_section.js
@@ -1,10 +1,3 @@
-const cardTitleHeight = "50";
-const [largeHeight, mediumHeight, smallHeight] = [
-    [550, 275, 275, 275, 350, 200],
-    [550, 275, 275, 275, 250, 250],
-    200
-];
-
 let cardsContent = {
     firstCard: {
         featureIndex: "feature-one",
@@ -38,63 +31,14 @@ let cardsContent = {
     }
 };
 
-let _heightValidation = (name, heightArr) => {
-    if (name === "feature-one") {
-        return heightArr[0];
-    } else if (name === "feature-two") {
-        return heightArr[1];
-    } else if (name === "feature-three") {
-        return heightArr[2];
-    } else if (name === "feature-four") {
-        return heightArr[3];
-    } else if (name === "feature-five") {
-        return heightArr[4];
-    } else if (name === "feature-six") {
-        return heightArr[5];
-    }
-};
-
-let getTranslateHeight = (name) => {
-    let windowWidth = Session.get("windowWidth");
-    if (windowWidth >= 993) {
-        return _heightValidation(name, largeHeight);
-    } else if (windowWidth < 993 && windowWidth > 600) {
-        return _heightValidation(name, mediumHeight);
-    } else if (windowWidth <= 600) {
-        return smallHeight;
-    }
-};
-
-// Template.featuresSection.events({
-//     "mouseenter .card": function(e, t) {
-//         let $target = $(e.target),
-//             targetHeight = $target.height();
-
-//         $target
-//             .find(".card-back")
-//             .css("height", targetHeight);
-//     },
-//     "mouseleave .card": function(e, t) {
-//         let $target = $(e.target)
-//         $target
-//             .find(".card-back")
-//             .css("height", cardTitleHeight);
-//     }
-// });
-
 Template.featuresSection.events({
     "mouseenter .card": function(e, t) {
-        let $target = $(e.target);
-        $target
+        $(e.target)
             .find(".card-back")
             .css("transform", "translateY(0)");
     },
     "mouseleave .card": function(e, t) {
-        let $target = $(e.target),
-            targetName = $target.attr("name"),
-            translateHeight = getTranslateHeight(targetName) - 70;
-
-        $target
+         $(e.target)
             .find(".card-back")
             .removeAttr("style");
     }
