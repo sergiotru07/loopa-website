02d51553d641885d9bd2fb97ee047bde4c864f4c
sergiotru07 <sergiotrujillo.a@gmail.com>
2016-01-08 15:15:49 -0500

    Create our team secction

---
+++ client/templates/website/our_team_section/our_team_section.js
@@ -0,0 +1,49 @@
+let ourTeamInfo = [{
+    coFounderName: "Andrés García",
+    coFounderRole: "Web Developer",
+    coFounderDescription: "Toffee chocolate cake tiramisu cupcake cake chocolate bar lemon drops. Dragée fruitcake dragée chocolate pastry. Macaroon candy canes candy canes caramels ice cream. Pudding fruitcake pastry dessert.",
+    imageBgPath: "our-team/bg_andres.jpg",
+    imagePerson:"our-team/andres.jpg",
+    facebookLink: "https://www.facebook.com/andresalejandro.garcia.12?ref=ts&fref=ts",
+    twitterLink: "",
+    googlePlusLink: "",
+    emailAdress: ""
+
+}, {
+    coFounderName: "Sergio Trujillo",
+    coFounderRole: "Web Designer",
+    coFounderDescription: "Toffee chocolate cake tiramisu cupcake cake chocolate bar lemon drops. Dragée fruitcake dragée chocolate pastry. Macaroon candy canes candy canes caramels ice cream. Pudding fruitcake pastry dessert.",
+    imageBgPath: "our-team/bg_sergio.png",
+    imagePerson:"our-team/Sergio.jpg",
+    facebookLink: "https://www.facebook.com/sergioandres.trujilloardila",
+    twitterLink: "",
+    googlePlusLink: "",
+    emailAdress: ""
+
+}, {
+    coFounderName: "Alex Realpe",
+    coFounderRole: "No sabe / No responde ??¿¿",
+    coFounderDescription: "Toffee chocolate cake tiramisu cupcake cake chocolate bar lemon drops. Dragée fruitcake dragée chocolate pastry. Macaroon candy canes candy canes caramels ice cream. Pudding fruitcake pastry dessert.",
+    imageBgPath: "our-team/bg_alex.jpg",
+    imagePerson:"our-team/alex.jpg",
+    facebookLink: "https://www.facebook.com/johnalex.realpe?fref=ts",
+    twitterLink: "",
+    googlePlusLink: "",
+    emailAdress: ""
+
+}, {
+    coFounderName: "Ricardo Vezga",
+    coFounderRole: "Graphic Designer",
+    coFounderDescription: "Toffee chocolate cake tiramisu cupcake cake chocolate bar lemon drops. Dragée fruitcake dragée chocolate pastry. Macaroon candy canes candy canes caramels ice cream. Pudding fruitcake pastry dessert.",
+    imageBgPath: "our-team/bg_ricardo.png",
+    imagePerson:"our-team/ricardo.jpg",
+    facebookLink: "https://www.facebook.com/ricardo.v.restrepo?fref=ts",
+    twitterLink: "",
+    googlePlusLink: "",
+    emailAdress: ""
+
+}];
+
+Template.ourTeamSection.helpers({
+    ourTeamInfo: () => ourTeamInfo
+});
